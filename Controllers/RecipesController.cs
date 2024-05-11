c8f86e0bdfca3b47071272ce3dd00bafd3cf47ff
Lera <valeronfor@mail.ru>
2024-05-11 22:44:46 +0600

    Рецептовый контроллер со странным неоптимизированным механизмом работы добавлен

--- Controllers/RecipesController.cs
+++ Controllers/RecipesController.cs
@@ -1,12 +1,44 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SmartFridgeAPI.DataBaseManagement;
+using SmartFridgeAPI.Models;
 
 namespace SmartFridgeAPI.Controllers
 {
+    [ApiController]
+    [Route("[controller]/[action]")]
     public class RecipesController : Controller
     {
-        public IActionResult Index()
+        [HttpGet, ActionName("GetAllRecipes")]
+        public Task<ActionResult<List<Рецепты>>> GetRecipes()
         {
-            return View();
+            return RecipesManager.GetRecipes();
+        }
+
+        // POST api/users
+        [HttpPost, ActionName("PostRecipe")]
+        public async Task<ActionResult<УпотребленныеПродукты>> PostRecipe(
+            int userId, string name, string description, List<НаборыПродуктов> products)
+        {
+            RecipesManager.addRecipe(userId, name, description, products);
+            return Ok();
+        }
+
+
+        //Удаление продукта  из холодильника
+        [HttpDelete, ActionName("DeleteRecipe")]
+        public IActionResult DeleteRecipe(int userId, string name)
+        {
+            RecipesManager.deleteRecipe(userId, name);
+            return NoContent();
+        }
+
+        // PUT api/users/
+        [HttpPut, ActionName("UpdateRecipe")]
+        public IActionResult UpdateRecipe(
+            int userId, string name, string description, List<НаборыПродуктов> products)
+        {
+            RecipesManager.changeRecipe(userId, name, description, products);
+            return Ok();
         }
     }
 }
--- DataBaseManagement/RecipesManager.cs
+++ DataBaseManagement/RecipesManager.cs
@@ -1,6 +1,109 @@
-﻿namespace SmartFridgeAPI.DataBaseManagement
+﻿using Microsoft.AspNetCore.Mvc;
+using SmartFridgeAPI.Models;
+
+namespace SmartFridgeAPI.DataBaseManagement
 {
     public class RecipesManager
     {
+        //Добавление рецепта (в списке ингредиентов idрецепта и idпродукта при передаче его в метод задаются произвольно)
+        public static async void addRecipe(int userId, string name, string description, List<НаборыПродуктов> products)
+        {
+            using (SmartFridgeContext db = new SmartFridgeContext())
+            {
+                int id = (from r in db.Рецептыs select r.IdРецепта).Max() + 1;
+                Рецепты reciepe = new Рецепты
+                {
+                    IdРецепта = id,
+                    Наименование = name,
+                    ПоследовательностьДействий = description,
+                    IdПользователя = userId
+                };
+                await db.Рецептыs.AddAsync(reciepe);
+                foreach (var item in products)
+                {
+                    item.IdРецепта = id;
+                    int pId=(from p in db.НаборыПродуктовs select p.IdПродукта).Max() + 1;
+                    item.IdПродукта = pId;
+                    await db.НаборыПродуктовs.AddAsync(item);
+                }
+                await db.SaveChangesAsync();
+            }
+        }
+
+        //Удаление рецепта
+        public static async void deleteRecipe(int userId, string name)
+        {
+            using (SmartFridgeContext db = new SmartFridgeContext())
+            {
+                var item = (from p in db.Рецептыs where p.IdПользователя == userId where p.Наименование==name select p).FirstOrDefault();
+                var productsList = (from pr in db.НаборыПродуктовs where pr.IdРецепта == item.IdРецепта select pr).ToList();
+                if (item != null)
+                {
+                    db.Рецептыs.Remove((Рецепты)item);
+                    await db.SaveChangesAsync();
+                }
+                if (productsList != null)
+                {
+                    foreach(var product in productsList)
+                    {
+                        db.НаборыПродуктовs.Remove(product);
+                    }
+                }
+                
+                await db.SaveChangesAsync();
+            }
+        }
+
+        //Изменение рецепта
+        public static async void changeRecipe(int userId, string name, string description, List<НаборыПродуктов> products)
+        {
+            using (SmartFridgeContext db = new SmartFridgeContext())
+            {
+                var item = (from p in db.Рецептыs where p.IdПользователя == userId where p.Наименование == name select p).FirstOrDefault();
+                var productsList = (from pr in db.НаборыПродуктовs where pr.IdРецепта == item.IdРецепта select pr).ToList();
+                if (item != null)
+                {
+                    db.Рецептыs.Remove((Рецепты)item);
+                    await db.SaveChangesAsync();
+                }
+                if (productsList != null)
+                {
+                    foreach (var product in productsList)
+                    {
+                        db.НаборыПродуктовs.Remove(product);
+                    }
+                }
+
+                await db.SaveChangesAsync();
+
+                Рецепты reciepe = new Рецепты
+                {
+                    IdРецепта = item.IdРецепта,
+                    Наименование = name,
+                    ПоследовательностьДействий = description,
+                    IdПользователя = userId
+                };
+                await db.Рецептыs.AddAsync(reciepe);
+                foreach (var it in products)
+                {
+                    it.IdРецепта = item.IdРецепта;
+                    int pId = (from p in db.НаборыПродуктовs select p.IdПродукта).Max() + 1;
+                    it.IdПродукта = pId;
+                    await db.НаборыПродуктовs.AddAsync(it);
+                }
+                await db.SaveChangesAsync();
+
+            }
+        }
+
+        //Получить рецепты все
+        public static async Task<ActionResult<List<Рецепты>>> GetRecipes()
+        {
+            using (SmartFridgeContext db = new SmartFridgeContext())
+            {
+                var recipeList = (from p in db.Рецептыs select p).ToList();
+                return new ActionResult<List<Рецепты>>(recipeList);
+            }
+        }
     }
 }
